3d2156b591ae17f6088f4f5840284222076a5cee
anas ben raies <a.benraies19038@pi.tn>
2024-08-22 08:46:18 +0100

    everything working but the useeefect

---
+++ src/Components/TheBarChart.jsx
@@ -0,0 +1,82 @@
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+// const data = [
+//   {
+//     name: 'Page A',
+//     pv: 2400,
+//     amt: 2400,
+//   },
+//   {
+//     name: 'Page B',
+//     pv: 1398,
+//     amt: 2210,
+//   },
+//   {
+//     name: 'Page C',
+//     pv: 9800,
+//     amt: 2290,
+//   },
+//   {
+//     name: 'Page D',
+//     pv: 3908,
+//     amt: 2000,
+//   },
+//   {
+//     name: 'Page E',
+//     pv: 4800,
+//     amt: 2181,
+//   },
+//   {
+//     name: 'Page F',
+//     pv: 3800,
+//     amt: 2500,
+//   },
+
+//   {
+//     name: 'Page G',
+//     pv: 4300,
+//     amt: 2100,
+//   },
+//   {
+//     name: 'Tomorrow',
+//     pv: 4300,
+//     amt: 2100,
+//   },
+
+  
+// ];
+
+export default class Example extends PureComponent {
+  static demoUrl = 'https://codesandbox.io/p/sandbox/simple-bar-chart-72d7y5';
+
+  render() {
+    const {data} = this.props;
+    return (
+      <div style={{ width: "45vw", height: '45vh' }}> 
+
+     
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="usageDate" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="durationInMinutes" fill="#17a2b860" activeBar={<Rectangle fill="pink" stroke="purple" />} />
+        </BarChart>
+      </ResponsiveContainer>
+      </div>
+    );
+  }
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setDevicesUsages } from '../../features/User'
 import { FaRegUserCircle } from "react-icons/fa";
 import "./Home.css"
-import { Container, Row, Col, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Alert, Dropdown } from 'react-bootstrap';
+import TheBarChart from '../../Components/TheBarChart';
+import TheLineChart from '../../Components/TheLineChart';
 
 /**
  * The Home function is the main component of the application.
@@ -15,16 +17,22 @@ import { Container, Row, Col, Alert } from 'react-bootstrap';
 function Home() {
   const dispatch = useDispatch()
   const currentUser = useSelector((state) => state.variables.currentUser)
+  // all device usages for the current user
   const devicesUsages = useSelector((state) => state.variables.devicesUsages)
   const [DevicesNumber, setDevicesNumber] = useState(0)
   const [LastDeviceUsage, setLastDeviceUsage] = useState(null)
   const [TotalThisMonth, setTotalThisMonth] = useState(0)
   const [devicesList, setDevicesList] = useState([])
-/**
- * Finds the latest device usage date and sets it as the last device usage.
- *
- * @return {void}
- */
+  const [SortedDevicesUsages, setSortedDevicesUsages] = useState([])
+  // these device usages to be shown by the Graph
+  const [SelectedDeviceUsages, setSelectedDeviceUsages] = useState([])
+  // this sets the device usages to be shown by the Graph grouped by date
+  const [DeviceUsagesForCharts, setDeviceUsagesForCharts] = useState([])
+  /**
+   * Finds the latest device usage date and sets it as the last device usage.
+   *
+   * @return {void}
+   */
   const LastDeviceUsageDate = () => {
     let LastDeviceUsage_date = devicesUsages[0]?.usageDate;
     let lastUsage = {}
@@ -48,10 +56,50 @@ function Home() {
     }
     console.log("total this month :", total)
     setTotalThisMonth(parseFloat(total).toFixed(2))
-
-
-
   }
+
+
+  /*
+   * Picks the device usages for the given device ID and sets it as the selected device usages.
+   *
+   * @param {number} deviceId - The ID of the device to pick usages for
+   * @return {void}
+   */
+  const PickDeviceUsages = (deviceId) => {
+    for (let deviceUsages of SortedDevicesUsages) {
+      if (deviceUsages[0].device.id === deviceId) {
+        setSelectedDeviceUsages(deviceUsages)
+        console.log("the selected device usages :", SelectedDeviceUsages)
+        break
+      }
+    }
+    getDeviceUsagesDurationAndEnergyUsageByDate()
+  }
+
+  const getDeviceUsagesDurationAndEnergyUsageByDate = () => {
+    const deviceUsagesByDate = SelectedDeviceUsages.reduce((groups, deviceUsage) => {
+      const {usageDate, durationInMinutes, energy_usage_in_kwh, device} = deviceUsage
+      const key = usageDate
+      if (!groups[key]) {
+        groups[key] = {
+          usageDate,
+          durationInMinutes: 0,
+          energy_usage_in_kwh: 0,
+          deviceId: device.id
+        }
+      }
+      groups[key].durationInMinutes += durationInMinutes
+      groups[key].energy_usage_in_kwh = groups[key].energy_usage_in_kwh + parseFloat(energy_usage_in_kwh)
+      return groups
+    }, {})
+
+    const deviceUsagesByDateList = Object.values(deviceUsagesByDate).slice(-7)
+    setDeviceUsagesForCharts(deviceUsagesByDateList)
+   
+    console.log("deviceUsagesByDateList", deviceUsagesByDateList)
+  }
+
+  
 
   // useEffect hook for data fetching the Device usage of the current user with his iD 
   useEffect(() => {
@@ -81,7 +129,12 @@ function Home() {
 
   useEffect(() => {
     /**
-     * Processes device usages by grouping them by device ID, sorting the grouped usages by date, and updating the devices number state.
+     * This function processes the device usages data by grouping it by device id and sorting it by date.
+     *
+     * It first checks if there are any device usages available. If there are, it calls the LastDeviceUsageDate and TotalEneryUse functions.
+     * Then, it groups the device usages by device id using the reduce function.
+     * After grouping, it converts the grouped object to an array of arrays and sorts each inner array by usage date.
+     * Finally, it sets the devices number and sorted devices usages in the state.
      *
      * @return {void}
      */
@@ -113,10 +166,28 @@ function Home() {
         groupedDevicesArray.forEach(deviceGroup => {
           deviceGroup.sort((a, b) => new Date(a.usageDate) - new Date(b.usageDate));
         });
-
         console.log("result after sorting", groupedDevicesArray);
-        console.log("length of the array", groupedDevicesArray.length);
-
+        console.log("length of the sorted array", groupedDevicesArray.length);
+        setSortedDevicesUsages(groupedDevicesArray)
+        console.log("sorted devices usages", SortedDevicesUsages)
+        // set the devices list 
+        if (groupedDevicesArray.length > 0) {
+          const devicesInfo = groupedDevicesArray.map(deviceUsages => ({
+            id: deviceUsages[0].device.id,
+            type: deviceUsages[0].device.type,
+            model: deviceUsages[0].device.model
+          }))
+          setDevicesList(devicesInfo)
+          console.log("devices list", devicesList)
+        }
+        // if ( groupedDevicesArray) {
+        //      for ( let deviceUsagesByDevice of groupedDevicesArray ){
+        //       if(deviceUsagesByDevice[0].device.id && deviceUsagesByDevice[0].device.type && deviceUsagesByDevice[0].device.model){
+        //         setDevicesList([...devicesList,{id:deviceUsagesByDevice[0].device.id,type:deviceUsagesByDevice[0].device.type,model:deviceUsagesByDevice[0].device.model}])
+        //       }
+        //      }
+        //      console.log("devices list",devicesList)
+        // }
 
         //sorting the grouped device usages by date
         // const sortedGroupedDevicesUsages = Object.entries(groupedDevicesUsages).map(([deviceId, deviceUsages]) => {
@@ -152,7 +223,7 @@ function Home() {
             <Row style={{ marginLeft: "-1.5vw" }}>
               <Col xs={6} md={4} >
                 <Alert variant={"info"} >
-                  Total Enery use this month : {TotalThisMonth} kWh
+                  Total Energy use this month : {TotalThisMonth} kWh
                 </Alert>
               </Col>
               <Col xs={6} md={4}>
@@ -174,23 +245,30 @@ function Home() {
         <h2>
           Device Monitoring
         </h2>
-        <div className="DeviceList">
-          <h4>Device : </h4>
-          <h4>Usage : </h4>
 
+        <div className="DeviceList">
+          <div className='DropdownDeviesList'><Dropdown>
+            <Dropdown.Toggle style={{ backgroundColor: "white", borderColor: "black",color:"black" }}>
+            {SelectedDeviceUsages.length>0?SelectedDeviceUsages[0]?.device?.type+"-"+SelectedDeviceUsages[0]?.device?.model+"-|ID:"+SelectedDeviceUsages[0]?.device?.id+"|":"Choose a device"}
+
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {devicesList.map((device, index) => (
+                <Dropdown.Item onClick={() => PickDeviceUsages(device.id)} key={index}>{device.type}-{device.model}-|ID:{device.id}|</Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          </div>
         </div>
       </div>
-      Home !
-      <h3>{currentUser.username}</h3>
-      <h3>{currentUser.email}</h3>
-      <div>DeviceUsages</div>
-      {devicesUsages.map((deviceUsage, index) => (
-        <div key={index}>
-          <p>Device : {deviceUsage.durationInMinutes}</p>
-          <p>Usage : {deviceUsage.device.type}</p>
-        </div>
-      ))}
-
+      <div className='ChartsTitles'>
+        <h4>Usage duration</h4>
+        <h4>Energy usage</h4>
+      </div>
+      <div className='Charts'>
+        <TheBarChart data={DeviceUsagesForCharts}/>
+        <TheLineChart data={DeviceUsagesForCharts}/>
+      </div>
     </div>
   )
 }
